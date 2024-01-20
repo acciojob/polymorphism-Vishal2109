545f9f9d96d12346830e8051bc1f570b09e40588
Vishal2109 <137199815+Vishal2109@users.noreply.github.com>
2024-01-20 11:22:49 +0530

    Add files via upload

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,4 +2,22 @@ package com.driver;
 
 public class Main {
 
+    static class Product{
+        public Product(){}
+
+        public int product(int x, int y) {return 0;}
+        public int product(int x, int y, int z) {return 0;}
+        public double product(double x, double y) {return 0.0d;}
+
+
+    }
+
+    public static void main(String[] args) {
+        Product p = new Product();
+
+        p.product(0,0);
+        p.product(0,0,0);
+        p.product(0.0d, 0.0d);
+
+    }
 }
